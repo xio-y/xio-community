3c4529035b54642f29c73e4d43ea2d03c6821222
xio <xio2k@foxmail.com>
2021-02-01 21:58:37 +0800

    正在实现注册逻辑

--- src/test/java/com/nowcoder/community/MapperTest.java
+++ src/test/java/com/nowcoder/community/MapperTest.java
@@ -5,10 +5,13 @@ import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.dao.UserMapper;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.MailClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import java.util.List;
 
@@ -51,5 +54,31 @@ public class MapperTest {
         System.out.println(list);
         int count=discussPostMapper.selectDiscussPostRows(0);
         System.out.println(count);
+    }
+
+    @Autowired
+    MailClient mailClient;
+
+    @Autowired
+    TemplateEngine templateEngine;
+
+    @Test
+    public void testMail(){
+        mailClient.sendMail("1599267511@qq.com","TEST","WELCOME");
+    }
+    @Test
+    public void testHtmlMail(){
+        Context context = new Context();
+        context.setVariable("username","用户");
+
+        String content=templateEngine.process("/mail/demo",context);
+        System.out.println(content);
+
+        mailClient.sendMail("1599267511@qq.com","HTML",content);
+    }
+
+    @Test
+    public void hahaha(){
+        System.out.println("abc".getBytes());
     }
 }
