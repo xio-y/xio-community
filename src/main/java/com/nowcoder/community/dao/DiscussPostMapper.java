b073de0d4d43d01b655c484ff8591bfeee035b71
xio <xio2k@foxmail.com>
2021-02-04 15:34:39 +0800

    在DiscussPostService使用spring提供的HtmlUtils类来防止XSS注入，完善主页上 “我要发布”按钮的逻辑，使之在未登录时不显示，修改index.js，增加异步请求逻辑

--- src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -9,8 +9,16 @@ import java.util.List;
 @Mapper
 public interface DiscussPostMapper {
 
-    List<DiscussPost> selectDiscussPosts(int userId,int offset,int limit);
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
 
-    //此注解用于取别名，在<if>中使用的唯一参数，必须加别名
-    int selectDiscussPostRows(@Param("userId")int userId);
+    // @Param注解用于给参数取别名,
+    // 如果只有一个参数,并且在<if>里使用,则必须加别名.
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+    int insertDiscussPost(DiscussPost discussPost);
+
+    DiscussPost selectDiscussPostById(int id);
+
+    int updateCommentCount(int id, int commentCount);
+
 }
