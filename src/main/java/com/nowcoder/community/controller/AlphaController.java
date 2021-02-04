367dc565c2d6ec962b57c8b12013e89668a2b156
xio <xio2k@foxmail.com>
2021-02-04 14:54:04 +0800

    引入ajax（异步js和xml），fastjson包

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -1,7 +1,9 @@
 package com.nowcoder.community.controller;
 
+import com.nowcoder.community.util.CommunityUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,5 +14,14 @@ public class AlphaController {
     @ResponseBody
     public String sayHello(){
         return "Hello SpringBoot.";
+    }
+
+    // ajax示例
+    @RequestMapping(path = "/ajax", method = RequestMethod.POST)
+    @ResponseBody
+    public String testAjax(String name, int age) {
+        System.out.println(name);
+        System.out.println(age);
+        return CommunityUtil.getJSONString(0, "操作成功!");
     }
 }
