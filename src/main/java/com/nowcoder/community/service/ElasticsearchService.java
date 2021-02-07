b5124d9a36058c5baded570b4e4b132d3ba951f0
xio <xio2k@foxmail.com>
2021-02-07 22:21:19 +0800

    使用ElasticSearch增加搜索帖子功能，并高亮关键词

---
+++ src/main/java/com/nowcoder/community/service/ElasticsearchService.java
@@ -0,0 +1,9 @@
+package com.nowcoder.community.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ElasticsearchService {
+
+
+}
---
+++ src/test/java/com/nowcoder/community/ElasticsearchTests.java
@@ -0,0 +1,135 @@
+package com.nowcoder.community;
+
+
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.elasticsearch.DiscussPostRepository;
+import com.nowcoder.community.entity.DiscussPost;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class ElasticsearchTests {
+
+    @Autowired
+    private DiscussPostMapper discussMapper;
+
+    @Autowired
+    private DiscussPostRepository discussRepository;
+
+    @Autowired
+    private ElasticsearchRestTemplate elasticsearchRestTemplate; // 注意不要注入ElasticsearchTemplate
+
+    @Test
+    public void testInsert() {
+        discussRepository.save(discussMapper.selectDiscussPostById(241));
+        discussRepository.save(discussMapper.selectDiscussPostById(242));
+        discussRepository.save(discussMapper.selectDiscussPostById(243));
+    }
+
+    @Test
+    public void testInsertList() {
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(101, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(102, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(103, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(111, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(112, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(131, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(132, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(133, 0, 100));
+        discussRepository.saveAll(discussMapper.selectDiscussPosts(134, 0, 100));
+    }
+
+    @Test
+    public void testUpdate() {
+        DiscussPost post = discussMapper.selectDiscussPostById(231);
+        post.setContent("我是新人,使劲灌水.");
+        discussRepository.save(post);
+    }
+
+    @Test
+    public void testDelete() {
+        // discussRepository.deleteById(231);
+        discussRepository.deleteAll();
+    }
+
+    @Test
+    public void testSearchByRepository() {
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.multiMatchQuery("互联网寒冬", "title", "content"))
+                .withSort(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("score").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+                .withPageable(PageRequest.of(0, 10))
+                .withHighlightFields(
+                        new HighlightBuilder.Field("title").preTags("<em>").postTags("</em>"),
+                        new HighlightBuilder.Field("content").preTags("<em>").postTags("</em>")
+                ).build();
+
+        // elasticTemplate.queryForPage(searchQuery, class, SearchResultMapper)
+        // 底层获取得到了高亮显示的值, 但是没有返回.
+
+        Page<DiscussPost> page = discussRepository.search(searchQuery);
+        System.out.println(page.getTotalElements());
+        System.out.println(page.getTotalPages());
+        System.out.println(page.getNumber());
+        System.out.println(page.getSize());
+        for (DiscussPost post : page) {
+            System.out.println(post);
+        }
+    }
+
+    @Test
+    public void testSearchByTemplate(){
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.multiMatchQuery("互联网寒冬", "title", "content"))
+                .withSort(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("score").order(SortOrder.DESC))
+                .withSort(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+                .withPageable(PageRequest.of(0, 10))
+                .withHighlightFields(
+                        new HighlightBuilder.Field("title").preTags("<em>").postTags("</em>"),
+                        new HighlightBuilder.Field("content").preTags("<em>").postTags("</em>")
+                ).build();
+        //elasticsearchTemplate.queryForPage()
+        //elasticsearchRestTemplate.queryForPage(searchQuery, DiscussPost.class, new SearchRe)
+
+        SearchHits<DiscussPost> search = elasticsearchRestTemplate.search(searchQuery, DiscussPost.class);
+        // 得到查询结果返回的内容
+        List<SearchHit<DiscussPost>> searchHits = search.getSearchHits();
+        // 设置一个需要返回的实体类集合
+        List<DiscussPost> discussPosts = new ArrayList<>();
+        // 遍历返回的内容进行处理
+        for(SearchHit<DiscussPost> searchHit : searchHits){
+            // 高亮的内容
+            Map<String, List<String>> highLightFields = searchHit.getHighlightFields();
+            // 将高亮的内容填充到content中
+            searchHit.getContent().setTitle(highLightFields.get("title") == null ? searchHit.getContent().getTitle() : highLightFields.get("title").get(0));
+            searchHit.getContent().setTitle(highLightFields.get("content") == null ? searchHit.getContent().getContent() : highLightFields.get("content").get(0));
+            // 放到实体类中
+            discussPosts.add(searchHit.getContent());
+        }
+        System.out.println(discussPosts.size());
+        for(DiscussPost discussPost : discussPosts){
+            System.out.println(discussPost);
+        }
+    }
+
+}
